913b6643e148ad2850b030642b6e3dbbb5846df4
geshan1234 <geshandhanodya1997@gmail.com>
2026-05-30 22:55:00 +0530

    Initial commit

---
+++ MyFirstApi.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstApi.Tests;
+
+using Xunit;
+
+public class SimpleTests
+{
+    [Fact]
+    public void Test_Should_Return_True()
+    {
+        Assert.True(true);
+    }
+}
---
+++ MyFirstApi/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("test")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(new[] { "User1", "User2" });
+        }
+    }
+}
